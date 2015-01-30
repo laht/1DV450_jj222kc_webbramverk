17c83a8104a669169d4e8f7b47122cd97123d4e2
Joakim Jonsson <jj222kc@student.lnu.se>
2015-01-30 00:10:23 +0000

    nu verkar git fungera som det ska, hann fixa lite med users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    def index
+        @User = User.first
+    end
+    def show
+        render :html => User.first(params[:id]) #glöm fan inte att ändra det här
+    end
 end
