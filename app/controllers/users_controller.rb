e2f2ed1f2a2a3f11682ca9998c2ea18110eda595
Joakim Jonsson <jj222kc@student.lnu.se>
2015-01-31 01:16:34 +0000

    minor user updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
     def index
-        @User = User.first
+        @user = User.all
     end
+    
+    def new
+        @user = User.new
+    end
+    
     def show
         render :html => User.first(params[:id]) #glöm fan inte att ändra det här
     end
+    
+    def create
+        u = User.new(user_params)
+
+        if u.save
+            redirect_to user_path(u)
+        end
+    end
+    
+    ##private methods
+	private
+	
+	def user_params
+		params.require(:user).permit(:name, :email, :password);
+	end
 end
