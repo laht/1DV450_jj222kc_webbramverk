57c3b054523a35c53154046e630ea0c8edc86f22
laht <jon.joakim@gmail.com>
2015-02-09 01:43:06 +0100

    Started css and some validation fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
             if !is_admin
                 redirect_to auth_token_path                
             end
+
+            @users = User.all
         end
     end
     
@@ -13,11 +15,30 @@ class UsersController < ApplicationController
     
     def create
         u = User.new(user_params)
+        messages = Array.new
 
         if u.save
             Token.create user_id = u.id
             redirect_to auth_token_path
-        end
+        else
+            if params[:user][:name] == '' || 
+                params[:user][:name].nil?
+                messages.push 'Ange ett namn'
+            end
+            if params[:user][:email] == '' || 
+                params[:user][:email].nil?
+                messages.push 'Ange en epost'
+            end
+            if params[:user][:password] == '' || 
+                params[:user][:password].nil?
+                messages.push 'Ange ett lösenord'
+            end
+            if params[:user][:password] != 
+                params[:user][:password_confirmation]
+                messages.push 'Lösenorder överensstämmer inte'
+            end
+        end     
+        redirect_to new_user_path, :notice => messages
     end
     
 	private
